f0a1662085fc4421f25da84f9427a067c17a75d1
水谷 圭佑/MIZUTANI <mizutanikeisuke_ccs@jp.nttdata.com>
2026-03-23 19:01:38 +0900

    動作確認完了

---
+++ dbServer/src/main/java/app/dbserver/DbServerApplication.java
@@ -0,0 +1,13 @@
+package app.dbserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DbServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DbServerApplication.class, args);
+		System.out.println("Ç‚Ç¡ÇΩÅ[");
+	}
+}
---
+++ dbServer/src/test/java/app/dbserver/DbServerApplicationTests.java
@@ -0,0 +1,13 @@
+package app.dbserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DbServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
